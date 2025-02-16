4fc6c1051a667004ea7b3b26f73b5398f163e0a8
betheni <bethenipieris2003@gmail.com>
2025-02-16 17:39:13 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
+
+public class Node {
+    int data;
+    Node leftChild, rightChild;
+
+    public Node(int data) {
+        this.data = data;
+        leftChild = rightChild = null;
+    }
+}
+
+
